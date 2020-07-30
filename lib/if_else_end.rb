02dd0801b5f56e954aab1f4497c4255624cec850
Ange Batie <ange.batie@gmail.com>
2020-07-30 00:16:45 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -25,5 +25,10 @@ puts "Oh, and always wear sunscreen!"
 =end
 
 current_time = Time.now
-current_time.to_s
+current_time = current_time.to_i
 
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
